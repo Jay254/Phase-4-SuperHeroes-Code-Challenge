f7cdc398a22994256bc2a4e1fa76529d716d158d
Yegon Japheth Kiptoo <jyegon504@gmail.com>
2023-07-02 20:40:56 +0300

    Done HeroPowers controller logic

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,4 +1,23 @@
 class HeroPowersController < ApplicationController
+    before_action :set_hero
+  
     def create
+      power = Power.find(params[:power_id])
+      if power
+        hero_power = HeroPower.new(hero: @hero, power: power, strength: params[:strength])
+        if hero_power.save
+          render json: @hero, include: { powers: { only: [:id, :name, :description] } }
+        else
+          render json: { errors: hero_power.errors.full_messages }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: "Power not found" }, status: :not_found
+      end
     end
-end
+  
+    private
+  
+    def set_hero
+      @hero = Hero.find(params[:hero_id])
+    end
+end  
